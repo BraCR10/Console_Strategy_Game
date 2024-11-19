3e69898bcbd9bd221f678dc6ee45bd74b88148ab
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-11-19 14:45:35 -0600

    Player screen

---
+++ console_game/src/main/java/src/java/player/ServerConnectionManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src.java.player;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author Brian Ramirez
+ */
+public class ServerConnectionManager {
+    Socket player;
+    
+}
